c20ff7e207e196c3b5349998f7d746636d64f611
Vanshika Goyal <vanshikagoyal1704@gmail.com>
2024-07-13 23:41:38 +0530

    Added Sounds and Animations

--- game.js
+++ game.js
@@ -8,10 +8,12 @@ $(".btn").on ("click", function() {
 
     userClickedPattern.push(userChosenColour);
 
-    console.log(userClickedPattern);
+    // console.log(userClickedPattern);
+    playSound (userChosenColour);
+    animatePress (userChosenColour);
 });
 
-function nextSequence(){
+function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
 
@@ -19,7 +21,19 @@ function nextSequence(){
     gamePattern.push(randomChosenColour);
     
     $("#" + randomChosenColour ).fadeIn(100).fadeOut(100).fadeIn(100) ;
-    var audio = new Audio ("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+function playSound (name) {
+    var audio = new Audio ("sounds/" + name + ".mp3");
     audio.play();
 }
 
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    // used Javascript to remove the pressed class after a 100 milliseconds.
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
